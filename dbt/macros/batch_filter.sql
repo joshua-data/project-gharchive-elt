c22fcbd6dbb37752d28813192b332da5bfb985cd
joshua-data <joshuajkim413@gmail.com>
2026-06-09 21:27:09 +0900

    edited: batch_filter interval param for period-to-date windows

--- dbt/macros/batch_filter.sql
+++ dbt/macros/batch_filter.sql
@@ -1,25 +1,23 @@
-{% macro batch_filter(date_col='dt', start_date=none, end_date=none) %}
+{% macro batch_filter(date_col='dt', start_date=none, end_date=none, interval='day') %}
 
     {% if start_date and end_date %}
-
-        {{ date_col }} between date '{{ start_date }}' and date '{{ end_date }}'
-
+        {% set sdt = start_date | string %}
+        {% set edt = end_date   | string %}
     {% else %}
-
         {% set batch_start_date = (var('batch_start_date', '') or '') | string | trim %}
-        {% set batch_end_date = (var('batch_end_date',   '') or '') | string | trim %}
-        {% set batch_date  = (var('batch_date',       '') or '') | string | trim %}
-        
+        {% set batch_end_date   = (var('batch_end_date',   '') or '') | string | trim %}
+        {% set batch_date       = (var('batch_date',       '') or '') | string | trim %}
         {% if batch_start_date and batch_end_date %}
-            {{ date_col }} between date '{{ batch_start_date }}' and date '{{ batch_end_date }}'
+            {% set sdt = batch_start_date %}
+            {% set edt = batch_end_date %}
         {% elif batch_date %}
-            {% set batch_end_date = modules.datetime.datetime.strptime(batch_date, '%Y-%m-%d').date() %}
-            {% set batch_start_date = batch_end_date - modules.datetime.timedelta(days=1) %}
-            {{ date_col }} between date '{{ batch_start_date }}' and date '{{ batch_end_date }}'
+            {% set sdt = (modules.datetime.datetime.strptime(batch_date, '%Y-%m-%d').date() - modules.datetime.timedelta(days=1)) | string %}
+            {% set edt = batch_date %}
         {% else %}
             {{ exceptions.raise_compiler_error("batch_filter: no valid date parameters provided.") }}
         {% endif %}
-
     {% endif %}
+
+    {{ date_col }} between date_trunc(date '{{ sdt }}', {{ interval }}) and date '{{ edt }}'
 
 {% endmacro %}
--- dbt/macros/get_where_subquery.sql
+++ dbt/macros/get_where_subquery.sql
@@ -24,16 +24,23 @@
 
 {% macro replace_batch_dates(where_string) %}
 
-    {% set batch_start_date = (var('batch_start_date', '') or var('batch_date', '')) | string | trim %}
-    {% set batch_end_date   = (var('batch_end_date',   '') or var('batch_date', '')) | string | trim %}
+    {% set batch_start_date = (var('batch_start_date', '') or '') | string | trim %}
+    {% set batch_end_date   = (var('batch_end_date',   '') or '') | string | trim %}
+    {% set batch_date       = (var('batch_date',       '') or '') | string | trim %}
 
-    {% if not batch_start_date or not batch_end_date %}
-        {{ exceptions.raise_compiler_error("replace_batch_dates: no valid date parameters provided (batch_start_date/batch_end_date or batch_date).") }}
+    {% if batch_start_date and batch_end_date %}
+        {% set sdt = batch_start_date %}
+        {% set edt = batch_end_date %}
+    {% elif batch_date %}
+        {% set sdt = (modules.datetime.datetime.strptime(batch_date, '%Y-%m-%d').date() - modules.datetime.timedelta(days=1)) | string %}
+        {% set edt = batch_date %}
+    {% else %}
+        {{ exceptions.raise_compiler_error("replace_batch_dates: no valid date parameters provided.") }}
     {% endif %}
 
     {% set result = where_string
-        | replace("__batch_start_date__", "date '" ~ batch_start_date ~ "'")
-        | replace("__batch_end_date__",   "date '" ~ batch_end_date   ~ "'") %}
+        | replace("__batch_start_date__", "date '" ~ sdt ~ "'")
+        | replace("__batch_end_date__",   "date '" ~ edt ~ "'") %}
     {{ return(result) }}
 
 {% endmacro %}
